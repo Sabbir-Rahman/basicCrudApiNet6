dc10a80e7ddd83af203fa2daae91184c25fd86af
sabbir rahman <sabbir.rahman@kaz.com.bd>
2022-06-01 15:59:23 +0600

    connection string setup

---
+++ basicCrudApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace basicCrudApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        } 
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeType> EmployeeTypes { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
