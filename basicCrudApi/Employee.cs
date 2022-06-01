29b62da5c99b15bfc99ce53021b5486829f3fba7
sabbir rahman <sabbir.rahman@kaz.com.bd>
2022-06-01 13:37:43 +0600

    basic model created

---
+++ basicCrudApi/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace basicCrudApi
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Comments { get; set; } = string.Empty;
+        public int EmployeeTypeId { get; set; }
+        public EmployeeType? EmployeeType { get; set; }
+    }
+}
---
+++ basicCrudApi/EmployeeType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace basicCrudApi
+{
+    public class EmployeeType
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string EmployeeName { get; set; } = string.Empty;
+    }
+}
