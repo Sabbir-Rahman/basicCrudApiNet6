379442875f62b352d856f4158f7814d5e1e585b1
sabbir rahman <sabbir.rahman@kaz.com.bd>
2022-06-03 17:28:52 +0600

    contrtoller api done

---
+++ basicCrudApi/Controllers/EmployeeTypesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using basicCrudApi;
+using basicCrudApi.Data;
+
+namespace basicCrudApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeTypesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EmployeeTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EmployeeTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeType>>> GetEmployeeTypes()
+        {
+          if (_context.EmployeeTypes == null)
+          {
+              return NotFound();
+          }
+            return await _context.EmployeeTypes.ToListAsync();
+        }
+
+        // GET: api/EmployeeTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeType>> GetEmployeeType(int id)
+        {
+          if (_context.EmployeeTypes == null)
+          {
+              return NotFound();
+          }
+            var employeeType = await _context.EmployeeTypes.FindAsync(id);
+
+            if (employeeType == null)
+            {
+                return NotFound();
+            }
+
+            return employeeType;
+        }
+
+        // PUT: api/EmployeeTypes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployeeType(int id, EmployeeType employeeType)
+        {
+            if (id != employeeType.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employeeType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EmployeeTypes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<EmployeeType>> PostEmployeeType(EmployeeType employeeType)
+        {
+          if (_context.EmployeeTypes == null)
+          {
+              return Problem("Entity set 'DataContext.EmployeeTypes'  is null.");
+          }
+            _context.EmployeeTypes.Add(employeeType);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployeeType", new { id = employeeType.Id }, employeeType);
+        }
+
+        // DELETE: api/EmployeeTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployeeType(int id)
+        {
+            if (_context.EmployeeTypes == null)
+            {
+                return NotFound();
+            }
+            var employeeType = await _context.EmployeeTypes.FindAsync(id);
+            if (employeeType == null)
+            {
+                return NotFound();
+            }
+
+            _context.EmployeeTypes.Remove(employeeType);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeTypeExists(int id)
+        {
+            return (_context.EmployeeTypes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
